bcb9e13153494b73016f208149ca50d3bd38e683
amitbbbb <126574686+amitbbbb@users.noreply.github.com>
2025-05-02 13:30:56 +0300

    only successor left

--- ex1.py
+++ ex1.py
@@ -10,6 +10,7 @@ WALL = 99
 FLOOR = 98
 AGENT = 1
 GOAL = 2
+AGENT_ON_GOAL = 3 
 LOCKED_DOORS = list(range(40, 50))
 PRESSED_PLATES = list(range(30, 40))
 PRESSURE_PLATES = list(range(20, 30))
@@ -23,10 +24,13 @@ class PressurePlateProblem(search.Problem):
     def __init__(self, initial):
 
         self.map = initial
+        self.goal = self.find_goal(initial)
+        agent = self.find_agent(initial)
+        state = (initial, agent)
 
         """ Constructor only needs the initial state.
         Don't forget to set the goal or implement the goal test"""
-        search.Problem.__init__(self, initial)
+        search.Problem.__init__(self, state)
 
     def successor(self, state):
         """ Generates the successor states returns [(action, achieved_states, ...)]"""
@@ -34,13 +38,34 @@ class PressurePlateProblem(search.Problem):
 
     def goal_test(self, state):
         """ given a state, checks if this is the goal state, compares to the created goal state returns True/False"""
-        utils.raiseNotDefined()
+        return self.map[self.goal[0]][self.goal[1]] == AGENT_ON_GOAL
+            
 
     def h(self, node):
         """ This is the heuristic. It gets a node (not a state)
         and returns a goal distance estimate"""
-        utils.raiseNotDefined()
+        agent = node.state[1]
+        goal = self.goal
+        # Manhattan distance heuristic
+        return abs(agent[0] - goal[0]) + abs(agent[1] - goal[1])
 
+
+
+    def find_goal(self, map):
+        """ given a map, return it's goal position"""
+        for i, row in enumerate(map):
+            for j, cell in enumerate(row):
+                if cell == GOAL:
+                    return (i, j)
+        return None
+
+    def find_agent(self, map):
+        """ given a map, return the agent position"""
+        for i, row in enumerate(map):
+            for j, cell in enumerate(row):
+                if cell == AGENT:
+                    return (i, j)
+        return None
 
 def create_pressure_plate_problem(game):
     print("<<create_pressure_plate_problem")
